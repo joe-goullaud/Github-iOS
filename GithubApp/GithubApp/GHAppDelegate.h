8479b6ac51c40f0279fb3d88156b01c9e47183fd
Joe Goullaud <joe.goullaud@raizlabs.com>
2013-07-15 23:08:16 -0400

    Initial Project Commit.

---
+++ GithubApp/GithubApp/GHAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GHAppDelegate.h
+//  GithubApp
+//
+//  Created by Joe Goullaud on 7/15/13.
+//  Copyright (c) 2013 Raizlabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GHAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GithubApp/GithubApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GithubApp
+//
+//  Created by Joe Goullaud on 7/15/13.
+//  Copyright (c) 2013 Raizlabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "GHAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([GHAppDelegate class]));
+    }
+}
